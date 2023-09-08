fafae2b36d0ca0056a2d0ab66ef600b1ead21ef2
yehezkiel <yehezkielhaganta@gmail.com>
2023-09-09 00:31:39 +0700

    menambahkan folder/file

---
+++ script/script.js
@@ -0,0 +1,22 @@
+var inputNumber = document.getElementById("inputfiled");
+inputNumber.addEventListener("input", function() {
+    document.getElementById("sisi").innerHTML = document.fform.inputfiled.value + " cm";
+    document.getElementById("sisi2").innerHTML = document.fform.inputfiled.value + " cm";
+    
+});
+function jumlahkan(){
+    var action = document.fform.rumus.value;
+    var sisi =  document.fform.inputfiled.value;
+    if(action == 'k'){
+        document.getElementById("kalimat").innerHTML = "Keliling persegi";
+        document.getElementById("rumus").innerHTML = "S + S + S + S ";
+        document.getElementById("operator").innerHTML = sisi + "+" + sisi + "+" + sisi + "+" + sisi;
+        document.getElementById("hasil") .innerHTML = sisi*4;
+    }
+    else if(action == 'l'){
+        document.getElementById("kalimat").innerHTML = "Luas persegi";
+        document.getElementById("rumus").innerHTML = "S x S";
+        document.getElementById("operator").innerHTML = sisi + "x" + sisi;
+        document.getElementById("hasil").innerHTML = sisi*sisi;
+    }
+}
---
+++ style/coba.js
@@ -0,0 +1,8 @@
+j=2;
+for(let i=0; i<=10; i++){
+    if(j>0){
+        //console.log(i);
+    }
+    console.log(j);
+    j=5-(j*2);
+}
